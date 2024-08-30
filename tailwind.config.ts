95ba68ad86709f8294fa4dfd0305ea34e114007c
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-08-30 20:34:43 +0200

    fixing some paths to images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':  'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'heroBg': 'url("/backgrounds/bg-start.webp")',
+        'treeBg': 'url("/tree.png")',
       },
       keyframes: {
         'infinite-scroll': {
