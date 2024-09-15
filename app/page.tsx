6776232e8559abae24ce3bb04ae3a7da2eb0b988
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-09-15 16:05:04 +0200

    adding new hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,61 @@
 import Preview from "@/components/Preview";
 import { frontPagePreview } from "../lib/objects";
+import Hero from "@/components/Hero";
 
 
 
 export default function Home() {
   return (
     <main>
-      <div className="outer-white">
-        <div className="outer-black-thick">
-          <div className="outer-white-thin">
-            <div className="outer-black-thin">
-           
-              <div className="flex flex-col justify-between h-[85svh] bg-heroBg bg-pos relative bg-no-repeat bg-cover bg-center rounded-[22px]">
-                              
-                <div className="flex relative justify-center items-center flex-col w-full md:w-[40%] md:ml-0 py-8 px-12   ">
+      
+      <div className="bg-secondary-pattern bg-cover bg-no-repeat bg-center w-screen h-screen">
 
-                  <div className="font-ibm text-primary-accent">                  
+      <Hero />
+      {/*
+            <div className="outer-white mr-8">
+              <div className="outer-black-thick">
+                <div className="outer-white-thin">
+                  <div className="outer-black-thin">
 
-                    <div>
-                      <h1 className="text-5xl m-0">WELCOME</h1>
-                      <p className="text-xl m-0">To Hannes Alexanderssons portfolio</p>
-                    </div>
-
-                    <div className="my-5 mx-0">
-                      This is where I store my collection of &rdquo;websites&rdquo; that I have done so far. Please feel free to look around. If you want to get in touch, dont hesitate to send me an e-mail or something!
-                    </div>
-                  </div>
+                    
                   
+                    <div className="flex flex-col justify-between h-[85svh] bg-heroBg bg-pos relative bg-no-repeat bg-cover bg-center rounded-[22px]">
+                                    
+                      <div className="flex relative justify-center items-center flex-col w-full md:w-[40%] md:ml-0 py-8 px-12   ">
 
-                </div>
-                <div className="flex justify-center items-center md:justify-start md:px-12 mb-12">
-                    <a className="font-grandstander" href='mailto:s103154@gmail.com?subject=I want you to come work for me!'>
-                      <span className="mailMe">
-                        <button className="btn-cta hover:bg-[#f3e1c7] hover:text-[#161415] hover:scale-105 bg-[#161415] text-[#f3e1c7] py-[10px] px-[20px] border-[2px] border-[#f45325] rounded-[5px] cursor-pointer">Get In Touch</button>
-                      </span>
-                    </a>
+                        <div className="font-ibm text-primary-accent">                  
+
+                          <div>
+                            <h1 className="text-5xl m-0">WELCOME</h1>
+                            <p className="text-xl m-0">To Hannes Alexanderssons portfolio</p>
+                          </div>
+
+                          <div className="my-5 mx-0">
+                            This is where I store my collection of &rdquo;websites&rdquo; that I have done so far. Please feel free to look around. If you want to get in touch, dont hesitate to send me an e-mail or something!
+                          </div>
+                        </div>
+                        
+
+                      </div>
+                      <div className="flex justify-center items-center md:justify-start md:px-12 mb-12">
+                          <a className="font-grandstander" href='mailto:s103154@gmail.com?subject=I want you to come work for me!'>
+                            <span className="mailMe">
+                              <button className="btn-cta hover:bg-[#f3e1c7] hover:text-[#161415] hover:scale-105 bg-[#161415] text-[#f3e1c7] py-[10px] px-[20px] border-[2px] border-[#f45325] rounded-[5px] cursor-pointer">Get In Touch</button>
+                            </span>
+                          </a>
+                      </div>
+                    </div>
+                     
+                  </div>
                 </div>
               </div>
-              
             </div>
-          </div>
-        </div>
+*/}
       </div>
       
       <div className="mt-20 mb-40 flex justify-center items-center flex-col ">
           
-          <div className="flex justify-center items-center mt-8 mb-4">
+          <div id="about" className="flex justify-center items-center mt-8 mb-4">
             <h2 className="text-5xl font-ibm text-[#f3e1c7]">Recent projects:</h2>
           </div>
           
---
+++ components/ComputerCanvas.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import {Suspense, useEffect, useState} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF  } from '@react-three/drei'
+import CanvasLoader from './CanvasLoader'
+
+interface ComputersProps {
+  isMobile: boolean;
+}
+
+
+const Computers:React.FC<ComputersProps> = ({ isMobile }) => {
+  const computer = useGLTF('/screen/scene.gltf')
+  return (
+    <mesh>
+       <hemisphereLight intensity={Math.PI} groundColor='black' />
+       <pointLight intensity={1}  />
+       <spotLight
+         position={[-20, 50, 10]}
+         angle={0.15}
+         penumbra={1}
+         intensity={1}
+         castShadow
+         shadow-mapSize={1024}
+        />
+       <primitive
+        object={computer.scene}
+        scale={isMobile ? 5 : 8}
+        position={isMobile ? [0, -3, -2.2] : [0, -8.25, -1.5]}
+        rotation={[0, 0, 0]}
+         />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches)
+    }
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+  
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 50 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate={false}
+          enableZoom={false}
+          maxPolarAngle={2}
+          minPolarAngle={2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
